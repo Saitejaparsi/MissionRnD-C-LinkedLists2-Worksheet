de8194823f2012e2fe228c970e196123225d0f55
Saitejaparsi <Saitejaparsi@missionrnd.com>
2016-03-19 23:15:54 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -24,7 +24,20 @@ int linkedListMedian(struct node *head) {
 		return -1;
 	else
 	{
-		struct node *i, *j;
+		if (head->next == NULL)
+			return head->num;
+		else if (head->next->next == NULL)
+			return (head->num + head->next->num)/2;
+		struct node *i = head, *j = head->next->next;
+		while (j != NULL)
+		{
+			if (j->next == NULL)
+				return i->next->num;
+			else if (j->next->next == NULL)
+				return (i->next->num + i->next->next->num) / 2;
+			else
+				i = i->next, j = j->next->next;
+		}
 
 	}
 }
